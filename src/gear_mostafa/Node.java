35adc5c5a13fd7921810504c01b5e8b25734e8b7
Youssef-Elkady <100090883+Youssef-Elkady@users.noreply.github.com>
2023-05-07 15:01:46 +0300

    Add files via upload

---
+++ src/gear_mostafa/Node.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package gear_mostafa;
+
+/**
+ *
+ * @author taver
+ */
+public interface Node {
+    public Node getParentNode();
+    public void setParentNode(Node node);
+}
